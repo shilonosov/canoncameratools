31e121ad1825fedcacff2c9c3116309f675920bc
shilonosov <shilonosov@514282d6-2cb6-5dad-956d-eb0125ace50c>
2010-11-16 21:26:25 +0000

    started to work on the LiveViewThread

--- trunk/noisymouse/Source/LiveViewThread.cs
+++ trunk/noisymouse/Source/LiveViewThread.cs
@@ -2,10 +2,47 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 
 namespace Source
 {
-    class LiveViewThread
+    public class LiveViewThread
     {
+        private CameraInfo _cameraInfo;
+        private CameraPool _cameraPool;
+        private bool _isRunning;
+        private Thread _thread;
+        private object _syncObject;
+
+        public LiveViewThread(CameraInfo cameraInfo, CameraPool cameraPool)
+        {
+            _cameraInfo = cameraInfo;
+            _cameraPool = cameraPool;
+            _isRunning = false;
+            _thread = new Thread(Worker);
+            _syncObject = new object();
+        }
+
+        private void Worker()
+        {
+            while (_isRunning)
+            {
+                LoopFunction();
+            }
+        }
+
+        protected void LoopFunction()
+        {
+            // TODO: do the magic
+        }
+
+        public void Start()
+        {
+
+        }
+
+        public void Stop()
+        {
+        }
     }
 }
